dd070b084ecf1ed8dac1e2c4638951e30271042b
ad4mCZ <biciste.adam@ssakhk.cz>
2024-03-30 14:25:22 +0100

    Osetril jsem roman/ucebnice, zadani zanru a (ne)nalezeni knihy kterou chces menit

--- src/Library.java
+++ src/Library.java
@@ -16,21 +16,40 @@ public class Library {
         String autor = reader.readLine();
         System.out.print("Zadejte rok vydání knihy: ");
         int publishYear = Integer.parseInt(reader.readLine());
+
+        int bookType = 0;
         System.out.print("Jedná se o román (1) nebo učebnici (2)?: ");
-        int bookType = Integer.parseInt(reader.readLine());
+        while (true) {
+            bookType = Integer.parseInt(reader.readLine());
+            if (bookType == 1 || bookType == 2) break;
+            else System.out.print("Zadejte 1 nebo 2: ");
+        }
         switch (bookType) {
             case 1:
-                System.out.print("Napište žánr (");
+                System.out.print("Vyberte žánr (");
                 for (Novel.Genre genre : Novel.Genre.values()) {
                     System.out.print(genre + ", ");
                 }
                 System.out.print("\b\b): ");
-                Novel.Genre genre = Novel.Genre.valueOf((reader.readLine().toLowerCase()));
-                //zkontrolovat jestli se jedna o zanr
-                Novel novel = new Novel(title, autor, publishYear, true, genre);
-                System.out.println("Přidán román: " + novel);
-                books.add(novel);
+
+                while (true) {
+                    String stringGenre = reader.readLine();
+
+                    boolean found = false;
+                    for (Novel.Genre genre : Novel.Genre.values()) {
+                        if (stringGenre.equalsIgnoreCase(String.valueOf(genre))) {
+                            Novel novel = new Novel(title, autor, publishYear, true, genre);
+                            System.out.println("Přidán román: " + novel.getName());
+                            books.add(novel);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) break;
+                    else System.out.print("Nenalezen žánr, zkus to prosím znovu: ");
+                }
                 break;
+
             case 2:
                 System.out.print("Napište vhodný ročník: ");
                 int grade = 0;
@@ -41,57 +60,51 @@ public class Library {
                 }
                 //sem asi pridat while
                 Textbook textbook = new Textbook(title, autor, publishYear, true, grade);
-                System.out.println("Přidána učebnice: " + textbook);
+                System.out.println("Přidána učebnice: " + textbook.getName());
                 books.add(textbook);
                 break;
         }
-
-        //System.out.println("Knihu '" + title + "' od autora/ů " + autor + ", vydanou v roce " + publishYear + ", jste úspěšně přidali.");
     }
 
     public static void editBook() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (Book book : books) {
-            System.out.printf("%s\n",book.getName());
+            System.out.printf("%s\n", book.getName());
         }
-        System.out.print("vyberte si jakou knihu chcete změnit: ");
-        String choice = reader.readLine();
+        boolean endLoop = true;
+        while (endLoop) {
+            System.out.print("vyberte si jakou knihu chcete změnit: ");
+            String choice = reader.readLine();
 
-        boolean found = false;
-        for (Book book : books) {
-            if (book.getName().equalsIgnoreCase(choice)) {
-                found = true;
-                System.out.println("nalezeno\n----------------------");
+            for (Book book : books) {
+                if (book.getName().equalsIgnoreCase(choice)) {
+                    System.out.println("nalezeno\n----------------------");
 
-                // Zadání nových parametrů
-
-                System.out.printf("Změnit název knihy [%s]: ", book.getName());
-                String newName = reader.readLine();
-                if (!newName.isEmpty()) {
-                    book.setName(newName);
-                }
-
-                System.out.printf("Změnit autora/y knihy [%s]", book.getAutor());
-                String newAutor = reader.readLine();
-                if (!newAutor.isEmpty()) {
-                    book.setAutor(newAutor);
-                }
-
-                System.out.printf("Změnit rok vydání knihy [%d]: ", book.getPublishYear());
-                String newPublishYear = reader.readLine();
-                try {
-                    if (!newPublishYear.isEmpty()) {
-                        book.setPublishYear(Integer.parseInt(newPublishYear));
+                    System.out.printf("Změnit název knihy [%s]: ", book.getName());
+                    String newName = reader.readLine();
+                    if (!newName.isEmpty()) {
+                        book.setName(newName);
                     }
-                }
-                catch (NumberFormatException e) {
-                    System.out.println("Neplatný formát ročníku. Zůstává původní rok.");
-                }
 
-                break;
+                    System.out.printf("Změnit autora/y knihy [%s]: ", book.getAutor());
+                    String newAutor = reader.readLine();
+                    if (!newAutor.isEmpty()) {
+                        book.setAutor(newAutor);
+                    }
+
+                    System.out.printf("Změnit rok vydání knihy [%d]: ", book.getPublishYear());
+                    String newPublishYear = reader.readLine();
+                    try {
+                        if (!newPublishYear.isEmpty()) {
+                            book.setPublishYear(Integer.parseInt(newPublishYear));
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Neplatný formát ročníku. Zůstává původní rok.");
+                    }
+                    endLoop = false;
+                    // Zadání nových parametrů
+                } else System.out.println("Nenalezeno, zkus to prosím znovu");
             }
-            else System.out.println("nenalezeno");
         }
-
     }
 }
